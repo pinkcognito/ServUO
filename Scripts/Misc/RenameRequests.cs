1be4ef65913e3ea7e7f905e425759ff525cebaea
unknown <drkprnce@yahoo.com>
2026-07-03 16:54:45 -0400

    Remove all pet name validation
    Was restricted to 1-16 chars, letters + up to 5 spaces only (no
    digits/punctuation), a reserved-prefix list, and a profanity filter.
    Now accepts any non-empty name.

--- Scripts/Misc/RenameRequests.cs
+++ Scripts/Misc/RenameRequests.cs
@@ -21,39 +21,18 @@ namespace Server.Misc
             {
                 name = name.Trim();
 
-                var numExceptions = 0;
-                var exceptions = NameVerification.Empty;
-
-                if (targ is BaseCreature)
-                {
-                    exceptions = new char[] { ' ' };
-                    numExceptions = 5;
-                }
-
-                if (NameVerification.Validate(name, 1, 16, true, false, true, numExceptions, exceptions, NameVerification.StartDisallowed, (Core.ML ? NameVerification.Disallowed : new string[] { })))
-                {
-                    if (Core.ML)
-                    {
-                        string[] disallowed = ProfanityProtection.Disallowed;
-
-                        for (int i = 0; i < disallowed.Length; i++)
-                        {
-                            if (name.IndexOf(disallowed[i]) != -1)
-                            {
-                                from.SendLocalizedMessage(1072622); // That name isn't very polite.
-                                return;
-                            }
-                        }
-
-                        from.SendLocalizedMessage(1072623, String.Format("{0}\t{1}", targ.Name, name)); // Pet ~1_OLDPETNAME~ renamed to ~2_NEWPETNAME~.
-                    }
-
-                    targ.Name = name;
-                }
-                else
+                if (name.Length == 0)
                 {
                     from.SendMessage("That name is unacceptable.");
+                    return;
                 }
+
+                if (Core.ML)
+                {
+                    from.SendLocalizedMessage(1072623, String.Format("{0}\t{1}", targ.Name, name)); // Pet ~1_OLDPETNAME~ renamed to ~2_NEWPETNAME~.
+                }
+
+                targ.Name = name;
             }
         }
     }
